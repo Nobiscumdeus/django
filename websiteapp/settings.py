8aeeeffcc10ff23e2d8332a15c8065617ec23c17
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-07-28 16:42:55 +0100

    removed surplus @ paths in requirements.txt file

--- websiteapp/settings.py
+++ websiteapp/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'aan3opke99(i!dsrdrzlu!je!wqf&4yos9%@f%#^f7=&qpu&*a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 #The * here is to alllow all hosts
-ALLOWED_HOSTS = ['127.0.0.1','dictionary22.herokuapp.com']
-
+#ALLOWED_HOSTS = ['127.0.0.1','nobiscumdeus2022.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
