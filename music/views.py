92758398699c3475c78f1bead3010f078d5d9008
Emmanuel Adeola <emmanueladeola990@gmail.com>
2022-11-02 10:26:47 +0100

    new updates

--- music/views.py
+++ music/views.py
@@ -15,7 +15,7 @@ def favorite(request,album_id):
     album=get_object_or_404(Album,pk=album_id)
     try:
         selected_song=album.song_set.get(pk=request.POST['song'])
-    except(KeyError,SongDoesNotExist):
+    except(KeyError,Song.DoesNotExist):
         return render(request,'music/detail.html',{
             'album':album,
             'error_message':"You did not select a valid song "
